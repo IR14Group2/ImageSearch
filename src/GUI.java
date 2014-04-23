c46fed1ee2dc1cf9a86c72ca1a0000baf133eba1
carolinemagnusson <caroline.magnusson.5@gmail.com>
2014-04-23 18:58:37 +0200

    Showing pictures

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,21 @@
-import java.io.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import org.apache.solr.common.SolrDocumentList;
 
 /**
  * Basic GUI for the ImageSearch application.
@@ -10,11 +24,13 @@ public class GUI extends JFrame implements ActionListener {
 
   // GUI configurations
   private final String TITLE = "ImageSearch";
-  private final int WIDTH = 400;
-  private final int HEIGHT = 400;
+  private final int WIDTH = 600;
+  private final int HEIGHT = 600;
+  private final int IMAGE_SIZE = 100;
+  private final int PICTURES_WIDTH = WIDTH/IMAGE_SIZE;
+  private final int MAX_PIC_WIDTH = 5;
+  
 
-  // URL parser
-  UrlParser urlParser;
 
   // Index
   Index index;
@@ -26,12 +42,13 @@ public class GUI extends JFrame implements ActionListener {
   JMenuItem jIndexUrlMenuItem;
   JTextField jSearchField;
   JButton jSearchButton;
+  JPanel resultPanel;
+//  JTextArea jResult;
 
   /**
    * Constructor for the GUI.
    */
   public GUI() {
-    urlParser = new UrlParser();
     index = new Index();
   }
 
@@ -40,22 +57,34 @@ public class GUI extends JFrame implements ActionListener {
    */
   public void init() {
 
-    jMainPanel = new JPanel();
+    jMainPanel = new JPanel(new BorderLayout());
     jMenuBar = new JMenuBar();
     jFileMenu = new JMenu("File");
     jIndexUrlMenuItem = new JMenuItem("Index URL ...");
-    jSearchField = new JTextField(20);
+    JPanel searchPanel = new JPanel();
+    jSearchField = new JTextField(40);
     jSearchButton = new JButton("Search");
+    System.err.println(PICTURES_WIDTH);
+    resultPanel = new JPanel(new GridLayout(0, Math.min(MAX_PIC_WIDTH, PICTURES_WIDTH)));// Don't want more than MAX_PIC_WIDTH pictures in width
+//    jResult = new JTextArea();
+    
+
 
     // Set all action listeners
     setActionListeners();
 
-    // Add GUI components to the main panale
+    // Add GUI components to the main panel
     jFileMenu.add(jIndexUrlMenuItem);
+    searchPanel.add(jSearchField);
+    searchPanel.add(jSearchButton);
+    
     jMenuBar.add(jFileMenu);
-    jMainPanel.add(jSearchField);
-    jMainPanel.add(jSearchButton);
-    add(jMainPanel);
+    jMainPanel.add(searchPanel, BorderLayout.PAGE_START);
+    jMainPanel.add(resultPanel, BorderLayout.LINE_START);
+    
+    //Makes the main panel scrollable
+    JScrollPane scrollpane = new JScrollPane(jMainPanel);
+    add(scrollpane);
 
     // Configure basic GUI behaviour and display the GUI
     setTitle(TITLE);
@@ -69,24 +98,45 @@ public class GUI extends JFrame implements ActionListener {
 
   private void setActionListeners() {
     // URL parsing action listener
-    jIndexUrlMenuItem.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        JFileChooser fc = new JFileChooser();
-        fc.setCurrentDirectory(new File("."));
-        int returnVal = fc.showOpenDialog(GUI.this);
-        if(returnVal == JFileChooser.APPROVE_OPTION) {
-          File file = fc.getSelectedFile();
-          urlParser.parseUrlsFromFile(file);
-        }
-      }
-    });
+//    jIndexUrlMenuItem.addActionListener(new ActionListener() {
+//      @Override
+//      public void actionPerformed(ActionEvent e) {
+//        JFileChooser fc = new JFileChooser();
+//        fc.setCurrentDirectory(new File("."));
+//        int returnVal = fc.showOpenDialog(GUI.this);
+//        if(returnVal == JFileChooser.APPROVE_OPTION) {
+//          File file = fc.getSelectedFile();
+//          urlParser.parseUrlsFromFile(file);
+//        }
+//      }
+//    });
 
     // Search button action listener
     jSearchButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        index.search(jSearchField.getText());
+//        jMainPanel.add(new TextArea("Searching"));
+        SolrDocumentList documents = index.search(jSearchField.getText());
+        LinkedList<ImageIcon> img = index.getImages(documents);//This might be better to do asynchronous
+        LinkedList<String> info = index.getInformation(documents);
+                
+        //Clear resultPanel from previous results
+        resultPanel.removeAll();
+        
+        JLabel jLabel;
+        if(img == null){
+          System.err.println("Cannot find images");
+        }
+        //Add all pictures to the resultPanel
+        for (int i=0;i<img.size(); i++) {
+      jLabel = new JLabel(info.get(i), index.resizeImage(img.get(i),IMAGE_SIZE,IMAGE_SIZE), JLabel.CENTER);
+      jLabel.setHorizontalTextPosition(JLabel.CENTER);
+      jLabel.setVerticalTextPosition(JLabel.BOTTOM);
+          resultPanel.add( jLabel);
+    }
+        validate();
+        repaint();
+
       }
     });
   }
--- src/Index.java
+++ src/Index.java
@@ -1,13 +1,131 @@
 /**
  * Created by mac on 2014-04-15.
  */
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.LinkedList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
 public class Index {
+  String solrURL = "http://localhost:8983/solr";
+  SolrServer server;
 
   public Index() {
+    server = new HttpSolrServer(solrURL);
+  }
+  
+  /*
+   * Connects to SolrServer and retrieves results as SolrDocumentList. 
+   * Functions below can then be used to retrieve a list of images or a list of text associated with the image, using the
+   * SolrDocumentList
+   * To make this work, the solrServer has to be started
+   */
+
+  public SolrDocumentList search(String query) {
+    SolrQuery queryObject = new SolrQuery();
+    queryObject.setQuery(query);
+
+    try {
+      QueryResponse rsp = server.query(queryObject);
+      SolrDocumentList docs = rsp.getResults();
+      return docs;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+  
+  public LinkedList<String> getInformation(SolrDocumentList docs){
+    LinkedList<String> info = null;
+    try {
+      info = new LinkedList<String>();
+      for (SolrDocument doc : docs) {
+        String name = (String) doc.getFieldValue("name");
+        info.add(name);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return info;
+  }
+  
+  /*
+   * Retrieves all the images from the documents in docs. For this to work, all resulting documents need a field called
+   * "url" that is the url to the image. 
+   * 
+   * Change this function if the images should be retrieved from a saved place locally.
+   */
+
+  public LinkedList<ImageIcon> getImages(SolrDocumentList docs) {
+    LinkedList<ImageIcon> pictures = null;
+    try {
+      pictures = new LinkedList<ImageIcon>();
+      for (SolrDocument doc : docs) {
+        String pictureURL = (String) doc.getFieldValue("url");
+        URL urlobj = new URL(pictureURL);
+        BufferedImage buffImage = ImageIO.read(urlobj);
+        ImageIcon img = new ImageIcon(buffImage);
+        pictures.add(img);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return pictures;
 
   }
-
-  public void search(String query) {
-    System.out.println("Search for query: " + query);
+  
+  public ImageIcon resizeImage(ImageIcon icon, int width, int height){
+    Image img = icon.getImage();  
+    Image newimg = img.getScaledInstance(width, height,  java.awt.Image.SCALE_SMOOTH);  
+    ImageIcon newIcon = new ImageIcon(newimg);
+    return newIcon;
   }
+
+//  public String connect(String query) throws Exception {
+//    URL url = new URL(urlString + query);
+//    URLConnection connection = url.openConnection();
+//    BufferedReader in = new BufferedReader(new InputStreamReader(
+//        connection.getInputStream()));
+//    String inputLine;
+//    StringBuffer returnString = new StringBuffer();
+//    while ((inputLine = in.readLine()) != null) {
+//      returnString.append(inputLine).append("\n");
+//    }
+//
+//    in.close();
+//    return returnString.toString();
+//  }
+
+  /*
+   * public String getURL(String xmlString){ try { DocumentBuilderFactory
+   * dbFactory = DocumentBuilderFactory.newInstance(); DocumentBuilder
+   * dBuilder = dbFactory.newDocumentBuilder(); Document doc =
+   * dBuilder.parse(xmlString); doc.getDocumentElement().normalize();
+   * 
+   * // System.out.println("root of xml file" +
+   * doc.getDocumentElement().getNodeName()); NodeList nodes =
+   * doc.getElementsByTagName("result"); //
+   * System.out.println("==========================");
+   * 
+   * for (int i = 0; i < nodes.getLength(); i++) { Node node = nodes.item(i);
+   * Element element = (Element) node;
+   * 
+   * } } catch (Exception ex) { ex.printStackTrace(); } }
+   * 
+   * private static String getValue(String tag, Element element) { NodeList
+   * nodes = element.getElementsByTagName(tag).item(0).getChildNodes(); Node
+   * node = (Node) nodes.item(0); return node.getNodeValue(); }
+   */
+
 }
